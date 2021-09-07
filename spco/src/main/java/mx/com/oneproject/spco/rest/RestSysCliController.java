6478142c1d5e4332c4d5099a3c3135fa3593416e
OneProjColab01 <el_buen_robert@yahoo.com.mx>
2021-09-06 23:04:41 -0500

    consulta de cliente unico

--- spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCliController.java
+++ spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCliController.java
@@ -2,6 +2,7 @@ package mx.com.oneproject.spco.rest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -205,6 +206,95 @@ public class RestSysCliController {
 	 	 respuesta.setContenido(resultado);
 		 respuesta.setCr("00");
 		 respuesta.setDescripcion("Correcto");
+         return respuesta;
+    }
+
+
+	/**
+	 * Esta clase define el método de consulta de un cliente especifico
+	 * @author: Roberto Avila
+	 * @version: 01/09/2021/A
+	 * @see 
+	 */	
+    @GetMapping(path = {"/Cliente"})
+    public AnsSysCatCli  obtenerClientePorClave(@RequestParam(required = false, value = "Id") String cliente,
+    		                    HttpServletRequest peticion) {
+    // Validación de token
+    	String user;
+    	AnsSysCatCli respuesta = new AnsSysCatCli();
+    	String token = peticion.getHeader("Authorization");
+                                                                		System.out.print("\n\n + RestSysCliController token: " + token + "\n ");
+		if (token != null) {
+			user = Jwts.parser()
+					.setSigningKey("0neProj3ct")
+					.parseClaimsJws(token.replace("Bearer",  ""))
+					.getBody()
+					.getSubject();
+                                                            			System.out.print("\n\n + RestSysCliController Usuario: " + user + "\n ");
+		}	else	{
+			respuesta.setCr("99");
+			respuesta.setDescripcion("Petición sin token");		
+			return respuesta;
+			}
+	// parametros empresa y recinto del usuario
+		SysUsuarios usuarioProc = sysUser.findByExiste(BigDecimal.valueOf(Double.valueOf(user)));
+		BigDecimal recinto = usuarioProc.getIdRecinto();
+		BigDecimal empresa = usuarioProc.getIdEmpresa();
+		String recintoS = recinto.toString();
+		String empresaS = empresa.toString();
+	// Preparación de la paginación.
+//		boolean enabled = true;
+//		SysCatCli sysCatCliCero = new SysCatCli();
+//		Long todos = (long) 0;
+//		double paginas = (float) 0.0;
+//		Integer pagEntero = 0;
+//		List<SysCatCli> todosSysCatCli;
+//		List<SysCatCli> paginaSysCatClientes; 
+//		Integer sysCatCliInicial, sysCatCliFinal;
+//		
+//		SysCatCliPag resultado = new SysCatCliPag();
+    //                                                                     	System.out.print(" + RestSysCliController listarPag page: " + page + " perpage: " + perPage + " tipo: " + tipo  +"\n ");
+    // obtener el total de sys_usuarios
+		 
+		 Optional<SysCatCli> todos = sysCli.findById(cliente);
+//         paginas = (double) todos / perPage;
+//         pagEntero = (int) paginas;
+//         if ((paginas-pagEntero)>0)
+//         {
+//        	 pagEntero++;
+//         }
+//    // Obtener la lista de sys_usuarios solicitada 
+//         sysCatCliInicial = (perPage  * (page - 1) );
+//         sysCatCliFinal   = (sysCatCliInicial + perPage) - 1;
+//         todosSysCatCli  = sysCli.findByTipos(tipo); //,empresa,recinto);
+//         paginaSysCatClientes = sysCli.findByTipos(tipo);   //,empresa,recinto);
+//         paginaSysCatClientes.clear();
+//         for (int i=0; i<todos;i++) {
+//        	 															System.out.print("\n " + "          + RestSysCliController Apendice: " + i + " - " + todosSysCatCli.get(i).getEmpresa());
+//        	 if(i>=sysCatCliInicial && i<=sysCatCliFinal)
+//        	 {
+//        		 sysCatCliCero = todosSysCatCli.get(i);
+//        		 paginaSysCatClientes.add(sysCatCliCero);
+//        		 System.out.print("  -- En lista  --" + sysCatCliCero.getIdCliProv());
+//        	 }
+//         }
+//         
+//         																System.out.print("\n + RestSysCliController listarPag todos: " + todos + " paginas: " + paginas + "  " + (paginas-pagEntero ) +"\n ");
+//         //
+//         resultado.setPage(page);
+//         resultado.setPerPage(perPage);
+//         resultado.setTotal((int) sysCli.countByTipos(tipo));   // , empresa, recinto));	
+//         resultado.setTotalPages(pagEntero);
+//         resultado.setSysCatClientes(paginaSysCatClientes);
+		 if (todos.isPresent()) {
+			 respuesta.setContenido(todos.get());
+			 respuesta.setCr("00");
+			 respuesta.setDescripcion("Correcto");
+		 } else
+		 {
+			 respuesta.setCr("98");
+			 respuesta.setDescripcion("No existe el cliente");			 
+		 }
          return respuesta;
     }
 
