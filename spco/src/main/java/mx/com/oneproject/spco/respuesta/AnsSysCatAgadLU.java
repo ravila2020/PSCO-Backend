63cd200e86aa9a3fd6ef4c1da31f0420e789ccb0
OneProjColab01 <el_buen_robert@yahoo.com.mx>
2021-10-08 00:21:02 -0500

    Lista de claves de agentes aduanales

---
+++ spco/src/main/java/mx/com/oneproject/spco/respuesta/AnsSysCatAgadLU.java
@@ -0,0 +1,30 @@
+package mx.com.oneproject.spco.respuesta;
+
+import java.util.List;
+
+public class AnsSysCatAgadLU {
+
+	private String cr;
+	private String descripcion;
+	private List<String> contenido;
+	
+	public String getCr() {
+		return cr;
+	}
+	public void setCr(String cr) {
+		this.cr = cr;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public List<String> getContenido() {
+		return contenido;
+	}
+	public void setContenido(List<String> contenido) {
+		this.contenido = contenido;
+	}
+	
+}
--- spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCatAgadController.java
+++ spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCatAgadController.java
@@ -1,6 +1,7 @@
 package mx.com.oneproject.spco.rest;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +23,7 @@ import mx.com.oneproject.spco.modelo.SysUsuarios;
 import mx.com.oneproject.spco.repositorio.IMSysCatAgadRepo;
 import mx.com.oneproject.spco.repositorio.IMSysUserRepo;
 import mx.com.oneproject.spco.respuesta.AnsSysCatAgad;
+import mx.com.oneproject.spco.respuesta.AnsSysCatAgadLU;
 import mx.com.oneproject.spco.respuesta.AnsSysCatAgagList;
 import mx.com.oneproject.spco.respuesta.SysCatAgadPag;
 
@@ -245,6 +247,92 @@ public class RestSysCatAgadController {
          resultado.setTotalPages(pagEntero);
          resultado.setSysAgads(paginaSysCatAgads);
 	 	 respuesta.setContenido(resultado);
+		 respuesta.setCr("00");
+		 respuesta.setDescripcion("Correcto");
+         return respuesta;
+    }
+	
+	/**
+	 * Esta clase define el método de consulta paginada de agentes
+	 * @author: Roberto Avila
+	 * @version: 0721/10/2021/A
+	 * @see 
+	 */	
+    @GetMapping(path = {"/Lista"})
+    public AnsSysCatAgadLU listarAgad( HttpServletRequest peticion) {
+    // Validación de token
+    	int page=1;
+    	int perPage=9999;
+    	String user;
+    	AnsSysCatAgadLU respuesta = new AnsSysCatAgadLU();
+    	String token = peticion.getHeader("Authorization");
+                                                                		System.out.print("\n\n + RestSysCatAgadController token: " + token + "\n ");
+		if (token != null) {
+			user = Jwts.parser()
+					.setSigningKey("0neProj3ct")
+					.parseClaimsJws(token.replace("Bearer",  ""))
+					.getBody()
+					.getSubject();
+                                                            			System.out.print("\n\n + RestSysCatAgadController Usuario: " + user + "\n ");
+		}	else	{
+			respuesta.setCr("99");
+			respuesta.setDescripcion("Petición sin token");		
+			return respuesta;
+			}
+	// parametros empresa y recinto del usuario
+		SysUsuarios agadProc = sysUser.findByExiste(BigDecimal.valueOf(Double.valueOf(user)));
+		BigDecimal recinto = agadProc.getIdRecinto();
+		BigDecimal empresa = agadProc.getIdEmpresa();
+		String recintoS = recinto.toString();
+		String empresaS = empresa.toString();
+		empresaS = String.format("%04d", empresa.intValue());
+		recintoS = String.format("%04d", recinto.intValue());
+	// Preparación de la paginación.
+		boolean enabled = true;
+		SysCatAgad SysCatAgadCero = new SysCatAgad();
+		Long todos = (long) 0;
+		double paginas = (float) 0.0;
+		Integer pagEntero = 0;
+		List<SysCatAgad> todosSysCatAgad;
+		List<SysCatAgad> paginaSysCatAgads; 
+		Integer SysCatAgadInicial, SysCatAgadFinal;
+		ArrayList<String> inicialListaERC = new ArrayList();
+		
+		SysCatAgadPag resultado = new SysCatAgadPag();
+                                                                      	System.out.print(" + RestSysCatAgadController listarPag page: " + page + " perpage: " + perPage +"\n ");
+    // obtener el total de sys_usuarios
+         todos = catAgad.countByER(empresaS,recintoS);
+         paginas = (double) todos / perPage;
+         pagEntero = (int) paginas;
+         if ((paginas-pagEntero)>0)
+         {
+        	 pagEntero++;
+         }
+    // Obtener la lista de sys_usuarios solicitada 
+         SysCatAgadInicial = (perPage  * (page - 1) );
+         SysCatAgadFinal   = (SysCatAgadInicial + perPage) - 1;
+         todosSysCatAgad  = catAgad.findByER(empresaS,recintoS);
+         paginaSysCatAgads = catAgad.findByER(empresaS,recintoS);
+         paginaSysCatAgads.clear();
+         for (int i=0; i<todos;i++) {
+//        	 															System.out.print("\n " + "          + RestSysCatAgadController Apendice: " + i + " - " + todosSysCatAgad.get(i).getClveProduc() + " - " + todosSysCatAgad.get(i).getTipProd() + " - " + todosSysCatAgad.get(i).getDescCorIng());
+        	 if(i>=SysCatAgadInicial && i<=SysCatAgadFinal)
+        	 {
+        		 SysCatAgadCero = todosSysCatAgad.get(i);
+        		 paginaSysCatAgads.add(SysCatAgadCero);
+        		 inicialListaERC.add(SysCatAgadCero.getNumPat());
+//        		 System.out.print("  -- En lista  --" + SysCatAgadCero.gegetClveProduc() );
+        	 }
+         }
+         
+         																System.out.print("\n + RestSysCatAgadController listarPag todos: " + todos + " paginas: " + paginas + "  " + (paginas-pagEntero ) +"\n ");
+         //
+         resultado.setPage(page);
+         resultado.setPerPage(perPage);
+         resultado.setTotal((int) catAgad.countByER(empresaS,recintoS));
+         resultado.setTotalPages(pagEntero);
+         resultado.setSysAgads(paginaSysCatAgads);
+	 	 respuesta.setContenido(inicialListaERC);
 		 respuesta.setCr("00");
 		 respuesta.setDescripcion("Correcto");
          return respuesta;
