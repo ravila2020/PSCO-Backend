4402c7c41c67aaa2265c18049cc8cf6749bd1e6c
OneProjColab01 <el_buen_robert@yahoo.com.mx>
2021-10-07 20:48:40 -0500

    Consulta de agente por Clave de patente

--- spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCatAgadController.java
+++ spco/src/main/java/mx/com/oneproject/spco/rest/RestSysCatAgadController.java
@@ -1,6 +1,8 @@
 package mx.com.oneproject.spco.rest;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.jsonwebtoken.Jwts;
 import mx.com.oneproject.spco.exception.ApiRequestException;
 import mx.com.oneproject.spco.modelo.SysCatAgad;
+import mx.com.oneproject.spco.modelo.SysUsuarios;
 import mx.com.oneproject.spco.repositorio.IMSysCatAgadRepo;
+import mx.com.oneproject.spco.repositorio.IMSysUserRepo;
 import mx.com.oneproject.spco.respuesta.AnsSysCatAgad;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
@@ -25,7 +30,10 @@ public class RestSysCatAgadController {
 
 	@Autowired
 	private IMSysCatAgadRepo catAgad;
-	
+
+	@Autowired
+	private IMSysUserRepo sysUser;
+
 	
 	/**
 	 * Esta clase define el método de consulta plana del catalogo de agentes aduanales
@@ -51,22 +59,14 @@ public class RestSysCatAgadController {
 		
 									System.out.print("\n\n + RestSysCatAgadController Alta: " + peticion.getRequestURI() + " " + peticion.getRequestURL()+ "\n ");	
 									System.out.print("\n\n + RestSysCatAgadController Alta: " + peticion.getHeader("Authorization")+ "\n ");	
-			//-----------
-			String recinto = NuevoAgad.getRecinto();
-			String empresa = NuevoAgad.getEmpresa();
-			empresa = String.format("%04d", Integer.valueOf(empresa));
-			recinto = String.format("%04d", Integer.valueOf(recinto));
-			NuevoAgad.setRecinto(recinto);
-			NuevoAgad.setEmpresa(empresa);
-			
-			//-----------
-			  // Validación de token    	
+ 	  // Validación de token    	
 			AnsSysCatAgad respuesta = new AnsSysCatAgad();
+			String user = new String();
 			String llave = new String();
 	    	String token = peticion.getHeader("Authorization");
 	                                                                		System.out.print("\n\n + RestSysCatAgadController token: " + token + "\n ");
 			if (token != null) {
-				String user = Jwts.parser()
+				  user = Jwts.parser()
 						.setSigningKey("0neProj3ct")
 						.parseClaimsJws(token.replace("Bearer",  ""))
 						.getBody()
@@ -77,6 +77,13 @@ public class RestSysCatAgadController {
 				respuesta.setDescripcion("Petición sin token");		
 				return respuesta;
 				}
+			SysUsuarios usuarioProc = sysUser.findByExiste(BigDecimal.valueOf(Double.valueOf(user)));
+			BigDecimal recinto = usuarioProc.getIdRecinto();
+			BigDecimal empresa = usuarioProc.getIdEmpresa();
+			String recintoS = recinto.toString();
+			String empresaS = empresa.toString();
+			empresaS = String.format("%04d", empresa.intValue());
+			recintoS = String.format("%04d", recinto.intValue());
 							
 	    	try {
 		    	//-------------existe el producto?
@@ -93,6 +100,64 @@ public class RestSysCatAgadController {
 					respuesta.setCr("83");
 					respuesta.setDescripcion("Ya existe agente");
 			        return respuesta;
+			   	  }
+		    	} catch (Exception ex) {
+		    		throw new ApiRequestException("Upsi");
+		    	}
+	}
+
+	/**
+	 * Esta clase define el método de consulta de agentes aduanales
+	 * @author: Roberto Avila
+	 * @version: 07/10/2021/A
+	 * @see 
+	 */	
+	@GetMapping(path = {"/ConsAgente"})
+	public AnsSysCatAgad consSysCatAgad(@RequestParam(required = false, value = "numPat") String numPat,
+			                        HttpServletRequest peticion){
+		
+									System.out.print("\n\n + RestSysCatAgadController Alta: " + peticion.getRequestURI() + " " + peticion.getRequestURL()+ "\n ");	
+									System.out.print("\n\n + RestSysCatAgadController Alta: " + peticion.getHeader("Authorization")+ "\n ");	
+			  // Validación de token    	
+			AnsSysCatAgad respuesta = new AnsSysCatAgad();
+			String user = new String();
+			String llave = new String();
+    		llave=numPat;
+	    	String token = peticion.getHeader("Authorization");
+	                                                                		System.out.print("\n\n + RestSysCatAgadController token: " + token + "\n ");
+			if (token != null) {
+			      user = Jwts.parser()
+						.setSigningKey("0neProj3ct")
+						.parseClaimsJws(token.replace("Bearer",  ""))
+						.getBody()
+						.getSubject();
+	                                                            			System.out.print("\n\n + RestSysCatAgadController Usuario: " + user + "\n ");
+			}	else	{
+				respuesta.setCr("99");
+				respuesta.setDescripcion("Petición sin token");		
+				return respuesta;
+				}
+			SysUsuarios usuarioProc = sysUser.findByExiste(BigDecimal.valueOf(Double.valueOf(user)));
+			BigDecimal recinto = usuarioProc.getIdRecinto();
+			BigDecimal empresa = usuarioProc.getIdEmpresa();
+			String recintoS = recinto.toString();
+			String empresaS = empresa.toString();
+			empresaS = String.format("%04d", empresa.intValue());
+			recintoS = String.format("%04d", recinto.intValue());
+							
+	    	try {
+		    	//-------------existe el producto?
+				if (catAgad.findById(llave).isEmpty()){
+		    	//-------------
+					respuesta.setCr("83");
+					respuesta.setDescripcion("No existe agente");
+			        return respuesta;
+			     } else {
+					Optional<SysCatAgad> AgadProc = catAgad.findById(llave);
+					respuesta.setCr("00");
+					respuesta.setDescripcion("Correcto");
+					respuesta.setContenido(AgadProc.get());
+					return respuesta;
 			   	  }
 		    	} catch (Exception ex) {
 		    		throw new ApiRequestException("Upsi");
